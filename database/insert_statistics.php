fd7c0ab61a528832daeaf9c3451fd7bb3395bae1
adavidbermeo <adavidbermeo@gmail.com>
2019-08-21 16:44:44 -0500

    4:44pm

--- database/insert_statistics.php
+++ database/insert_statistics.php
@@ -1,4 +1,31 @@
 <?php
-$con = mysqli_connect('127.0.0.1','root','','') or die('Connection Error');
+class Database{
+    //Attributes 
+    protected $host;
+    protected $username;
+    protected $password;
+    protected $database;
 
+    protected $con;
+    protected $sql;
+    protected $result;
+
+
+    //Methods
+    public function __construct($host,$username,$password,$database){
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+    public function connectDatabase(){
+        $this->con = mysqli_connect($this->host,$this->username,$this->password, $this->database);
+    }
+    public function 
+}
+
+$con = mysqli_connect('127.0.0.1','root','','fb_project') or die('Connection Error');
+$sql = "INSERT INTO 'account' VALUES ()";
+
+$res = mysqli_query($con, $sql) or die('');
 ?>
