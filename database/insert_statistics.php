a62e2c857aec04bb3af35889d6009d6f6fdf13b0
adavidbermeo <adavidbermeo@gmail.com>
2019-08-22 08:46:11 -0500

    8:46am

--- database/insert_statistics.php
+++ database/insert_statistics.php
@@ -21,30 +21,68 @@ class Database{
     public function connectDatabase(){
         $this->con = mysqli_connect($this->host,$this->username,$this->password, $this->database);
     }
-    public function action($crud){
+    public function action($action,$table,$fields = 0,$values = 0){
         /**
          * $crud = 'insert', 'select', 'update' or 'delete'
          */
-        switch ($crud) {
+        switch ($action) {
             case 'insert':
-                $this->sql = "INSERT INTO table() VALUES (),()";
-                // Answer
-                $this->result = mysqli_query($this->con, $this->sql) or die('');
+                for ($i=0; $i <count($values[$fields[0]]) ; $i++) { 
+                    foreach ($fields as $field) {
+                        if($i>1){
+                            $this->sql = "INSERT INTO '$table($field[$i])' VALUES (', $values[$field][$i]')";
+                            // Answer
+                            $this->result = mysqli_query($this->con, $this->sql) or die('No hubo inserción');
+                            
+                        }else{
+                            $this->sql = "INSERT INTO '$table($field[$i])' VALUES ('$values[$field][$i]')";
+                            // Answer
+                            $this->result = mysqli_query($this->con, $this->sql) or die('No hubo inserción');
+                        }
+                    }
+                }
+                if ($this->result) {
+                    echo "Se han insertado correctamente los datos";
+                }
                 break;
-            case 'select':
-                $this->sql = "SELECT * FROM 'table' WHERE 'id' = '$id'";
+
+            case 'especific_select':
+                $this->sql = "SELECT * FROM '$table' WHERE '$fields' = '$values'";
                 // Answer
-                $this->result = mysqli_query($this->con, $this->sql) or die('');
+                $this->result = mysqli_query($this->con, $this->sql) or die('No hubo consulta especifica');
+                if (mysqli_num_rows($this->result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($this->result)) {
+                        echo "Query: " . $row[$fields];
+                    }
+                }else{
+                    echo "0 results";
+                }
                 break;
-            case 'update':
-                $this->sql = "";
+            case 'general_select':
+                $this->sql = "SELECT * FROM '$table'";
                 // Answer
-                $this->result = mysqli_query($this->con, $this->sql) or die('');
+                $this->result = mysqli_query($this->con, $this->sql) or die('No hubo consulta general');
+                if (mysqli_num_rows($this->result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($this->result)) {
+                        $keys = array_keys($row);
+                        foreach ($keys as $key) {
+                            echo "Query: " . $row[$key];
+                        }
+                        
+                    }
+                }else{
+                    echo "0 results";
+                }
                 break;
             case 'delete':
-                $this->sql = "";
+                $this->sql = "DELETE FROM '$table' WHERE '$fields' = '$values'";
                 // Answer
-                $this->result = mysqli_query($this->con, $this->sql) or die('');
+                $this->result = mysqli_query($this->con, $this->sql) or die('No se elimino ningun registro');
+                if ($this->result) {
+                    echo "Se han eliminado correctamente los datos";
+                }
                 break;        
             default:
                 echo "There is no server answer";
@@ -53,8 +91,8 @@ class Database{
     } 
 }
 
-$con = mysqli_connect('127.0.0.1','root','','fb_project') or die('Connection Error');
-$sql = "INSERT INTO 'account' VALUES ()";
+// $con = mysqli_connect('127.0.0.1','root','','fb_project') or die('Connection Error');
+// $sql = "INSERT INTO 'account' VALUES ()";
 
-$res = mysqli_query($con, $sql) or die('');
+// $res = mysqli_query($con, $sql) or die('');
 ?>
