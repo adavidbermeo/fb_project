291388921957b967e2959929d49ed22d33a86080
adavidbermeo <adavidbermeo@gmail.com>
2019-11-25 09:42:40 -0500

    New Commit

--- dashboard/account_dashboard.php
+++ dashboard/account_dashboard.php
@@ -1,7 +1,7 @@
 <?php 
 // echo "Angel";
-    if(isset($_POST['data'])){
-        $selected = $_POST['data'];
+    if(isset($_POST['selected'])){
+        $selected = $_POST['selected'];
         list($url, $click) = explode('=', $selected);
         list($click_value,$data) = explode('*', $click);
         list($id_page, $ad_account_id) = explode('%', $data); 
@@ -21,8 +21,12 @@
         protected $result;
         protected $ad_account_id;
         protected $data_graphics;
+        protected $campaign_graphic;
+        protected $ad_graphic;
+        protected $graphic_array;
+        
 
-        public function __construct(){
+        public function __construct($ad_account_id){
             $this->db_host = 'localhost';
             $this->username = 'root';
             $this->password = '';
@@ -33,10 +37,10 @@
         public function connectDb(){
             $this->con = mysqli_connect($this->db_host, $this->username, $this->password, $this->database_name) or die('Connection has failed');
         }
-        public function requestData($ad_account_id){
+        public function requestData(){
             // First Request
 
-            $this->request = "SELECT `total_new_likes` FROM `page` WHERE `ad_account_id` = '$ad_account_id'";
+            $this->request = "SELECT `total_new_likes` FROM `page` WHERE `ad_account_id` = '$this->ad_account_id'";
             $this->result = mysqli_query($this->con, $this->request) or die('There was an error');
             
             if (mysqli_num_rows($this->result) > 0) {
@@ -50,7 +54,7 @@
             }
             // Second Request 
 
-            $this->request = "SELECT `interactions` FROM `ad` WHERE `ad_account_id` = '$ad_account_id'";
+            $this->request = "SELECT `interactions` FROM `ad` WHERE `ad_account_id` = '$this->ad_account_id'";
             $this->result = mysqli_query($this->con, $this->request) or die('There was an error');
 
             if(mysqli_num_rows($this->result) > 0 ){
@@ -61,11 +65,11 @@
                         $field = $key; 
                     }
                 }
-                 echo '<div id="dash_section"><b>' . $key . '</b><br>' . array_sum($interactions_array). '</div>';
+                 echo '<div id="dash_section"><b>' . $field . '</b><br>' . array_sum($interactions_array). '</div>';
             }
             // Third Request 
 
-            $this->request = "SELECT `cost_per_lead`,`spend` FROM `campaign` WHERE `ad_account_id` = '$ad_account_id'";
+            $this->request = "SELECT `cost_per_lead`,`spend` FROM `campaign` WHERE `ad_account_id` = '$this->ad_account_id'";
             $this->result = mysqli_query($this->con, $this->request) or die('There was an error');
 
             if(mysqli_num_rows($this->result) > 0 ){
@@ -81,12 +85,40 @@
             }
         }
         public function requestGraphics(){
-            $this->request = "SELECT `clicks` FROM `campaign` WHERE ad_account_id ='$ad_account_id' ORDER BY interactions DESC ";
+            $this->request = "SELECT `campaign_id`,`campaign_name`,`clicks` FROM `campaign` WHERE `ad_account_id` ='$this->ad_account_id' ORDER BY `clicks` DESC";
             $this->result = mysqli_query($this->con,$this->request) or die('There was an error');
+           
+            $this->campaign_graphic = mysqli_fetch_all($this->result ,MYSQLI_ASSOC);
+            // print_r($this->campaign_graphic);
+            //  if(mysqli_num_rows($this->result) > 0 ){
+
+            //     while($data = mysqli_fetch_array($this->result, MYSQLI_ASSOC)){
+            //         $this->campaign_graphic[] = $data;
+            //     }
+            //     //print_r($this->campaign_graphic);
+            //  }
+             
+            $this->request = "SELECT `ad_ids`,`ad_name`,`total_reactions` FROM `ad` WHERE `ad_account_id` ='$this->ad_account_id' ORDER BY `total_reactions` DESC";
+            $this->result = mysqli_query($this->con,$this->request) or die('There was an error');
+
+            $this->ad_graphic = mysqli_fetch_all($this->result, MYSQLI_ASSOC);
+            // print_r($data); die('jajaja');
+            // if(mysqli_num_rows($this->result) > 0 ){
+
+            //     while($data = mysqli_fetch_array($this->result, MYSQLI_ASSOC)){
+            //        $this->ad_graphic[] = $data;
+            //     }
+            //      //print_r($this->ad_graphic);
+            //  }
+            $this->graphic_array = [$this->ad_graphic, $this->campaign_graphic];
+            // print_r($this->graphic_array);
+            echo json_encode($this->graphic_array);
+            // echo '<div id="chart-container"></div>';
         }
     }
      function getData($ad_account_id){
         $db = new Database($ad_account_id);
         $db->connectDb();
         $db->requestData();
+        $db->requestGraphics();
     }
